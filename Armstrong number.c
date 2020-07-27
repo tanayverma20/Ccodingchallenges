29a50f59e3a40c89cb3e8abe76e08ae26f96a546
tanayverma20 <68861442+tanayverma20@users.noreply.github.com>
2020-07-27 20:31:22 +0530

    Add files via upload
    I am submitting my first program on git hub.
    It is related to Armstrong number.

---
+++ Armstrong number.c
@@ -0,0 +1,39 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+                              // ARMSTRONG NUMBER  //
+#include <stdio.h>
+int cube(int digit);
+int main()
+{
+    int limit,temp,rem,sum=0;
+    printf("Enter a limit:");
+    scanf("%d",&limit);
+    temp=n;
+    while(n!=0)
+    {
+        rem=n%10;
+        sum=sum+cube(rem);
+        n=n/10;
+    }
+    
+    if(temp==sum)
+     printf("Armstromg number");
+     else
+     printf("Not an armstrong number");
+    
+    
+     // int temp = cube(n);
+     // printf("%d ",temp);
+    
+    return 0;
+}
+
+int cube(int digit)
+{
+    return digit*digit*digit;
+}
